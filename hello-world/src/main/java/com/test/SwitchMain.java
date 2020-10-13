194307cff75d6f22c9a2afe4cf61e25483749286
Jirka Pinkas <jirka.pinkas@gmail.com>
2020-10-13 14:05:13 +0200

    switch

---
+++ hello-world/src/main/java/com/test/SwitchMain.java
@@ -0,0 +1,19 @@
+package com.test;
+
+public class SwitchMain {
+
+    public static void main(String[] args) {
+        int a = 10;
+        switch (a) {
+            case 10 :
+                System.out.println("desitka");
+                break;
+            case 20 :
+                System.out.println("dvacitka");
+                break;
+            default :
+                System.out.println("neco jineho");
+        }
+    }
+
+}
