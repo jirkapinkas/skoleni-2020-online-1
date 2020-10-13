cb1a5d682acdb36c7c77843178887a5180e80283
Jirka Pinkas <jirka.pinkas@gmail.com>
2020-10-13 12:57:03 +0200

    + immutable example II.

---
+++ hello-world/src/main/java/com/test/immutable/ImmutableAgain.java
@@ -0,0 +1,31 @@
+package com.test.immutable;
+
+import java.util.Arrays;
+
+public class ImmutableAgain {
+
+    private String[] strings;
+
+    public ImmutableAgain(String[] strings) {
+        this.strings = Arrays.copyOf(strings, strings.length);
+    }
+
+    public String[] getStrings() {
+        return Arrays.copyOf(strings, strings.length);
+    }
+
+    @Override
+    public String toString() {
+        return "NotImmutable{" +
+                "strings=" + Arrays.toString(strings) +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        String[] strings = {"a", "b", "c"};
+        ImmutableAgain xxx = new ImmutableAgain(strings);
+        strings[0] = "x";
+        xxx.getStrings()[0] = "y";
+        System.out.println(xxx);
+    }
+}
