38ade210c92f08ade3ae609dfeee9597c7140cae
Jirka Pinkas <jirka.pinkas@gmail.com>
2020-10-13 12:51:27 +0200

    + polymorfizmus

---
+++ hello-world/src/main/java/com/test/PolymorfizmusMain.java
@@ -0,0 +1,33 @@
+package com.test;
+
+import com.test.pojo.Dog;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class PolymorfizmusMain {
+
+    public static void main(String[] args) {
+        Dog dog = Dog.builder()
+                .name("Lassie")
+                .sex(Dog.Sex.FEMALE)
+                .race("Border Kolie")
+                .alive(true)
+                .build();
+        System.out.println(dog/*.toString()*/);
+
+        Date date = new Date(); // POZOR! STARY ZPUSOB PRACE S DATUMEM A CASEM!!!
+        int currentYearDeprecated = date.getYear();
+
+        int currentYear = LocalDate.now().getYear();
+
+        LocalDate monthStart = LocalDate.now()
+                .withDayOfMonth(1);
+        LocalDate monthEnd = LocalDate.now()
+                .withDayOfMonth(1)
+                .plusMonths(1)
+                .minusDays(1);
+        System.out.println("current month: " + monthStart + " - " + monthEnd);
+
+    }
+}
--- hello-world/src/main/java/com/test/pojo/Dog.java
+++ hello-world/src/main/java/com/test/pojo/Dog.java
@@ -2,6 +2,8 @@ package com.test.pojo;
 
 import lombok.*;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @Builder
@@ -23,4 +25,31 @@ public class Dog {
 
     private Sex sex;
 
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", race='" + race + '\'' +
+                ", weight=" + weight +
+                ", alive=" + alive +
+                ", sex=" + sex +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dog dog = (Dog) o;
+        return Double.compare(dog.weight, weight) == 0 &&
+                alive == dog.alive &&
+                Objects.equals(name, dog.name) &&
+                Objects.equals(race, dog.race) &&
+                sex == dog.sex;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, race, weight, alive, sex);
+    }
 }
