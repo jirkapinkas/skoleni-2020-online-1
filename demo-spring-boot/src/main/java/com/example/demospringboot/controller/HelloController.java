b469b57790de4fc5ede4e9eb1eb7c6490a443f7e
Jirka Pinkas <jirka.pinkas@gmail.com>
2020-10-14 15:20:49 +0200

    problem se synchronizaci

--- demo-spring-boot/src/main/java/com/example/demospringboot/controller/HelloController.java
+++ demo-spring-boot/src/main/java/com/example/demospringboot/controller/HelloController.java
@@ -9,13 +9,31 @@ import java.time.format.DateTimeFormatter;
 
 @RestController
 public class HelloController {
+//
+//    private int counter;
 
     // http://localhost:8080/hello
     @GetMapping("/hello")
     public Message hello() {
+//        counter++;
         return Message.builder()
                 .message("Ahoj Svete, Spring Boot funguje ... opravdu!")
                 .date(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss")))
                 .build();
     }
+//
+//    @GetMapping("/hello2")
+//    public Message hello2() {
+//        counter--;
+//        return Message.builder()
+//                .message("Ahoj Svete, Spring Boot funguje ... opravdu!")
+//                .date(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss")))
+//                .build();
+//    }
+//
+//    @GetMapping("/counter")
+//    public int counter() {
+//        return counter;
+//    }
+//
 }
